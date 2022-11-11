e084b175e32fb7a57018d5cd9932d22e065079e7
AndreNovikxv <andre.nemetz.18@gmail.com>
2022-11-11 07:14:30 -0300

    Teste técnico IONIC HEALTH

---
+++ MyFirstApp/Migrations/20221106215603_Categoria.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFirstApp.Migrations
+{
+    public partial class Categoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MyFirstApp/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstApp.Models
+{
+  public class Produto
+  {
+    public int Id { get; set; }
+    [Display(Name = "Personagem:")]
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public string Descricao { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+
+  }
+}
